8f2f0966a703d57b95116a54423f6cd388800fa6
aristotlema <aristotle.maule@hotmail.com>
2021-05-05 09:47:08 -0400

    init commit setup navbar - node-sass

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavBar from './NavBar';
+import './App.scss';
+
+const App = () => {
+    return <NavBar />;
+};
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h2 className="main-logo">
+                OakField
+            </h2>
+        </nav>
+    );
+};
+
+export default NavBar;
