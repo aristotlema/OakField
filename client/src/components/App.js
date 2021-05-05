890fb6521a36bd9d8f45eff92deada1554e82aa0
aristotlema <aristotle.maule@hotmail.com>
2021-05-05 12:41:03 -0400

    setup react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import NavBar from './NavBar';
+import HomePage from '../pages/HomePage/HomePage';
+import ProductsPage from '../pages/ProductsPage/ProductsPage';
+import AboutPage from '../pages/AboutPage/AboutPage';
 import './App.scss';
 
 const App = () => {
     return (
         <div className="main-container">
-            <NavBar />
+            <BrowserRouter>
+                <div>
+                    <NavBar />
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/products" exact component={ProductsPage} />
+                    <Route path="/about" exact component={AboutPage} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 };
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 const NavBar = () => {
     return (
         <nav className="top-navbar">
             <h2 className="main-logo">
-                OakField
+                <Link to="/">Oak Field</Link>
             </h2>
             <div class="main-navigation">
-                <a>Home</a>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/about">About</Link>
+                {/* <a>Home</a>
                 <a>About</a>
-                <a>Products</a>
+                <a>Products</a> */}
             </div>
             <div className="session-control">
                 <div>Cart</div>
