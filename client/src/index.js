e4625ae7832791b0241e0ca9634eb14bf9793acf
aristotlema <aristotle.maule@hotmail.com>
2021-05-05 12:13:47 -0400

    setup API - setup Redux and devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import App  from './components/App';
+import store from './state/store';
+
+const reduxStore = createStore(store, composeWithDevTools(applyMiddleware(reduxThunk)))
 
 ReactDOM.render(
-    <App />,
+    <Provider store={reduxStore} >
+            <App />
+    </Provider>,
     document.getElementById('root')
 );
